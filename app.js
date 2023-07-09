b4a56d6f698403a1b6d3ad8e3625bba194941cb8
Yeferur <99824587+Yeferur@users.noreply.github.com>
2023-07-09 16:23:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 const layout = require('express-ejs-layouts');
-const port = 3000;
+const port = 5000;
 const app = express();
 
 // Seteamos el motor de vistas
